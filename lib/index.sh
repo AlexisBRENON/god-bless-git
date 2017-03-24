5ec6e4f2333772aa6e0812a5fd03c9f33d683bbb
Alexis BRENON <brenon.alexis@gmail.com>
2017-03-24 16:06:13 +0100

    ✨  Report moved/renamed files
    Also implement some code to report ignored_files later

--- lib/index.sh
+++ lib/index.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-# TODO: handle renamed/moved files
-
 _gbg_get_index_status() {
   git_status="$(git status --porcelain 2> /dev/null)"
   gbg_index_modifications_num=$(echo "${git_status}" | grep -Ece '^M')
   gbg_index_has_modifications=$( [ "${gbg_index_modifications_num}" -gt 0 ] && echo "true" || echo "false" )
+
+  gbg_index_moves_num=$(echo "${git_status}" | grep -Ece '^R')
+  gbg_index_has_moves=$( [ "${gbg_index_moves_num}" -gt 0 ] && echo "true" || echo "false" )
 
   gbg_index_deletions_num=$(echo "${git_status}" | grep -Ece '^D')
   gbg_index_has_deletions=$( [ "${gbg_index_deletions_num}" -gt 0 ] && echo "true" || echo "false" )
@@ -15,6 +16,8 @@ _gbg_get_index_status() {
 
   export gbg_index_modifications_num
   export gbg_index_has_modifications
+  export gbg_index_moves_num
+  export gbg_index_has_moves
   export gbg_index_deletions_num
   export gbg_index_has_deletions
   export gbg_index_additions_num
--- lib/workspace.sh
+++ lib/workspace.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-# TODO: handle renamed/moved files
-
 _gbg_get_workspace_status() {
-  git_status="$(git status --porcelain 2> /dev/null)"
+  [ "${_gbg_list_untracked:-""}" = true ] && _gbg_ignored="--ignore" || _gbg_ignored=""
+  git_status="$(git status --porcelain "${_gbg_ignored}" 2> /dev/null)"
+  unset _gbg_ignored
+
   gbg_workspace_modifications_num=$(echo "${git_status}" | grep -Ece '^.M')
   gbg_workspace_has_modifications=$( [ "${gbg_workspace_modifications_num}" -gt 0 ] && echo "true" || echo "false" )
 
@@ -13,10 +14,15 @@ _gbg_get_workspace_status() {
   gbg_workspace_untracked_num=$(echo "${git_status}" | grep -Ece '^\?\?')
   gbg_workspace_has_untracked=$( [ "${gbg_workspace_untracked_num}" -gt 0 ] && echo "true" || echo "false" )
 
+  gbg_workspace_ignored_num=$(echo "${git_status}" | grep -Ece '^!!')
+  gbg_workspace_has_ignored=$( [ "${gbg_workspace_ignored_num}" -gt 0 ] && echo "true" || echo "false" )
+
   export gbg_workspace_modifications_num
   export gbg_workspace_has_modifications
   export gbg_workspace_deletions_num
   export gbg_workspace_has_deletions
   export gbg_workspace_untracked_num
   export gbg_workspace_has_untracked
+  export gbg_workspace_ignored_num
+  export gbg_workspace_has_ignored
 }
