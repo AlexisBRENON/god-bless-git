83387ac3ff2ad0277a70e2867d41cd255638181e
Alexis BRENON <alexis@keecker.com>
2018-07-10 17:53:39 +0200

    ✨  Fetch the pending action

--- lib/repo.sh
+++ lib/repo.sh
@@ -41,6 +41,13 @@ _gbg_get_repo_status() {
             echo "true" || \
             echo "false" )"
 
+        lgbg_pending_action_type="$(\
+          _gbg_repo_get_pending_action \
+          "${lgbg_git_dir}")"
+        lgbg_has_pending_action="$( \
+            [ -n "${lgbg_pending_action_type}" ] && \
+            echo "true" || \
+            echo "false" )"
     fi
 
     gbg_repo_git_dir="${lgbg_git_dir:-}"
@@ -50,6 +57,8 @@ _gbg_get_repo_status() {
     gbg_repo_has_stashes="${lgbg_has_stashes:-}"
     gbg_repo_conflicts_num="${lgbg_number_of_conflicts:-}"
     gbg_repo_has_conflicts="${lgbg_has_conflicts:-}"
+    gbg_repo_pending_action_type="${lgbg_pending_action_type:-}"
+    gbg_repo_has_pending_action="${lgbg_has_pending_action:-}"
 
     export gbg_is_a_git_repo
     export gbg_repo_git_dir
@@ -59,4 +68,41 @@ _gbg_get_repo_status() {
     export gbg_repo_has_stashes
     export gbg_repo_conflicts_num
     export gbg_repo_has_conflicts
+    export gbg_repo_pending_action_type
+    export gbg_repo_has_pending_action
 }
+
+_gbg_repo_get_pending_action() {
+    _git_dir=${1}
+    _pending_action=""
+    if [ -d "${_git_dir}/rebase-merge" ]; then
+        _pending_action="rebase"
+        if [ -f "${_git_dir}/rebase-merge/interactive" ]; then
+            _pending_action="${_pending_action}:interactive"
+        else
+            _pending_action="${_pending_action}:merge"
+        fi
+    elif [ -d "${_git_dir}/rebase-apply" ]; then
+        if [ -f "${_git_dir}/rebase-apply/rebasing" ]; then
+            _pending_action="rebase"
+        elif [ -f "${_git_dir}/rebase-apply/applying" ]; then
+            _pending_action="apply"
+        else
+            _pending_action="apply:rebase"
+        fi
+    elif [ -f "${_git_dir}/MERGE_HEAD" ]; then
+        _pending_action="merge"
+    elif [ -f "${_git_dir}/CHERRY_PICK_HEAD" ]; then
+        _pending_action="cherry-pick"
+    elif [ -f "${_git_dir}/BISECT_LOG" ]; then
+        _pending_action="bisect"
+    fi
+
+    echo "${_pending_action}"
+
+    unset _pending_action _git_dir
+}
+
+
+
+
